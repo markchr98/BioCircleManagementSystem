3f80fe48fdef511cbd0b34c32e87d47a774f11a1
André Robert Damgaard Gollubits <andr849a@edu.eal.dk>
2018-04-30 14:20:04 +0200

    Added code-behind for StorageView.

---
+++ BioCircleManagementSystem/ViewModels/StorageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioCircleManagementSystem.ViewModels
+{
+    class StorageViewModel
+    {
+        private static StorageViewModel instance;
+        //Constructor
+        private StorageViewModel()
+        {
+
+        }
+        public static StorageViewModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new StorageViewModel();
+                }
+                return instance;
+            }
+        }
+
+        public void NewMachine(int machineNumber, string machineType)
+        {
+            //DataManager.Instance.CreateCustomer(new Customer(customerName, billingAddress, billingZipcode, billingCity, installationAddress, installationZipcode, installationCity, economicsCustomerNumber));
+
+        }
+    }
+}
--- BioCircleManagementSystem/Views/Storage/StorageCreate.xaml.cs
+++ BioCircleManagementSystem/Views/Storage/StorageCreate.xaml.cs
@@ -24,5 +24,10 @@ namespace BioCircleManagementSystem.Views.Storage
         {
             InitializeComponent();
         }
+
+        private void Button_Click_StorageCreate(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
