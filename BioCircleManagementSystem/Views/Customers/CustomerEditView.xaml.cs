41d41eccad1ce778a582fd3b0ec92a345dded515
markchr98 <mark4@live.dk>
2018-05-07 12:54:46 +0200

    Edit customer view basics

---
+++ BioCircleManagementSystem/Views/Customers/CustomerEditView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioCircleManagementSystem.Model;
+using BioCircleManagementSystem.ViewModels;
+
+namespace BioCircleManagementSystem.Views.Customers
+{
+    /// <summary>
+    /// Interaction logic for CustomerEditView.xaml
+    /// </summary>
+    public partial class CustomerEditView : Page
+    {
+        CustomerFindViewModel customerFindViewModel;
+        public CustomerEditView()
+        {
+            InitializeComponent();            
+            customerFindViewModel = CustomerFindViewModel.Instance;
+            DataContext = customerFindViewModel;
+        }
+
+        public void Button_Click_RemoveContact(object sender, RoutedEventArgs e)
+        {
+            Button contact = ((Button)sender);
+            if (contact.DataContext is Contact deleteme)
+            {
+                customerFindViewModel.RemoveContact(deleteme);
+            }
+        }
+    }
+}
