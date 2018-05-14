d842e658d6f8d1a24ea766b9f50c95def2e90a5a
SezSahin <sezg0006@edu.eal.dk>
2018-05-14 14:56:56 +0200

    Created the OrderFind window.

---
+++ BioCircleManagementSystem/Views/Orders/OrderFind.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioCircleManagementSystem.ViewModels;
+
+namespace BioCircleManagementSystem.Views.Orders
+{
+    /// <summary>
+    /// Interaction logic for OrderFind.xaml
+    /// </summary>
+    public partial class OrderFind : Page
+    {
+        OrderCreateViewModel orderCreateViewModel;
+        
+        public OrderFind()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_SearchMachines(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void customers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //if statement to prevent endless loop caused by UnselectAll() causing a slectionChanged event
+            if (machines.SelectedItem != null)
+            {
+                //Might be changed to page with navigation in frame 
+                OrderCreateViewModel CEV = new OrderCreateViewModel();
+                ShellWindow window = new ShellWindow();
+                window.WindowState = WindowState.Normal;
+                window.Content = CEV;
+                window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                window.ShowDialog();
+                window.Height = 450;
+                window.Width = 900;
+                machines.UnselectAll();
+                //Unselect current item so that a selection changed does not happen again
+                //when exiting and re-entering the find customer page
+            }
+        }
+    }
+}
